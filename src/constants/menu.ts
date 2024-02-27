0fa26aa5d37588fe1eec5f9d78c8d387a63b9962
han <longcoutinho@gmail.com>
2024-02-27 17:22:08 +0700

    change menu title

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,8 +1,8 @@
-import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
+import {faCartShopping, faCoins} from "@fortawesome/free-solid-svg-icons";
 export const menuBar =
     [
         {
-            title: "product",
+            title: "Mua",
             icon: faCartShopping,
             child: [
                 {
@@ -19,4 +19,23 @@ export const menuBar =
                 }
             ]
         },
+        {
+            title: "Nap tien",
+            icon: faCoins,
+            child: [
+                {
+                    title: "categories",
+                    url: "/item/categories"
+                },
+                {
+                    title: "all products",
+                    url: "/item/all"
+                },
+                {
+                    title: "add product",
+                    url: "/item/add"
+                }
+            ]
+        },
+
     ];
