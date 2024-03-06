b6542110b900692ecff1bc4f51523fd7075d3b4e
maitho <maitho3101@gmail.com>
2024-03-06 16:56:25 +0700

    update UI

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,49 +1,52 @@
-import {faCartShopping, faCoins, faHistory, faContactBook} from "@fortawesome/free-solid-svg-icons";
-export const menuBar =
-    [
-        {
-            title: "Mua",
-            icon: faCartShopping,
-            child: [
-                {
-                    title: "Danh sách sản phẩm",
-                    url: "/buy"
-                },
-            ]
-        },
-        {
-            title: "Tài khoản",
-            icon: faCoins,
-            child: [
-                {
-                    title: "Nạp tiền",
-                    url: "/topup"
-                },
-            ]
-        },
-        {
-            title: "Lịch sử",
-            icon: faHistory,
-            child: [
-                {
-                    title: "Đơn hàng đã mua",
-                    url: "/sale-order/list"
-                },
-                {
-                    title: "Yêu cầu nạp tiền",
-                    url: "/topup/list"
-                },
-            ]
-        },
-        {
-            title: "Liên hệ",
-            icon: faContactBook,
-            child: [
-                {
-                    title: "Thông tin liên hệ",
-                    url: "/topup"
-                },
-            ]
-        },
-
-    ];
+import {
+  faCartShopping,
+  faCoins,
+  faHistory,
+  faContactBook,
+} from "@fortawesome/free-solid-svg-icons";
+export const menuBar = [
+  {
+    title: "Mua",
+    icon: faCartShopping,
+    child: [
+      {
+        title: "Danh sách sản phẩm",
+        url: "/buy",
+      },
+    ],
+  },
+  {
+    title: "Tài khoản",
+    icon: faCoins,
+    child: [
+      {
+        title: "Nạp tiền",
+        url: "/topup",
+      },
+    ],
+  },
+  {
+    title: "Lịch sử",
+    icon: faHistory,
+    child: [
+      {
+        title: "Đơn hàng đã mua",
+        url: "/sale-order/list",
+      },
+      {
+        title: "Yêu cầu nạp tiền",
+        url: "/topup/list",
+      },
+    ],
+  },
+  {
+    title: "Liên hệ",
+    icon: faContactBook,
+    child: [
+      {
+        title: "Thông tin liên hệ",
+        url: "/info",
+      },
+    ],
+  },
+];
